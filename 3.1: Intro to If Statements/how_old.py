157d1b60919e700175a421cf26214396ee084cc9
E-Kat101 <74022900+E-Kat101@users.noreply.github.com>
2023-03-02 20:32:56 -0500

    Update how_old.py

--- 3.1: Intro to If Statements/how_old.py
+++ 3.1: Intro to If Statements/how_old.py
@@ -1,1 +1,14 @@
+name = input("Hey, what's you name? ")
+age = int(input(f"Okay, {name}, how old are you? "))
 
+if age < 16:
+    print(f"You can't drive, {name}.")
+
+if age < 18:
+    print(f"You can't vote, {name}.")
+
+if age < 21:
+    print(f"You can't rent a car, {name}.")
+
+if age >= 21:
+    print(f"Congrats, {name}, you can do anything that's legal.")
