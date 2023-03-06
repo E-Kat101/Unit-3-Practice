fb106432c167a68c66daea2cd9fb99038b9fb88e
E-Kat101 <74022900+E-Kat101@users.noreply.github.com>
2023-03-06 00:53:56 -0500

    Create two_more_questions.py

---
+++ 3.3 Boolean Operators/two_more_questions.py
@@ -0,0 +1,25 @@
+print("TWO MORE QUESTIONS :D")
+print("------------------")
+print("Think of something and I'll try to guess it!")
+print("  ")
+
+q1 = input("Question 1) Does it belong inside or outside or both? ")
+q2 = input("Question 2) Is it a living thing? ")
+
+if q1 == "inside" and q2 == "yes":
+    print("I think you're thinking of the soul that's trapped inside of that creepy doll called Annabelle.")
+
+if q1 == "inside" and q2 == "no":
+    print("I think you're thinking of the houseplant that you haven't watered in weeks.")
+
+if q1 == "outside" and q2 == "yes":
+    print("I think you're thinking of Patrick from SpongeBob SquarePants.")
+
+if q1 == "outside" and q2 == "no":
+    print("I think you're thinking of a pebble. Just a very tiny, little rock.")
+
+if q1 == "both" and q2 == "yes":
+    print("I think you're thinking of a child.")
+
+if q1 == "both" and q2 == "no":
+    print("I think you're thinking of my long-lost hopes and dreams ;-;")
